8e2ff18e1e8406013bfde80a3cfabfa92e8e5e9a
gordonmenzies <heymish96@gmail.com>
2024-03-27 16:40:11 +0000

    recreate Initial Wireframe

--- Punk_Api_Project/src/App.tsx
+++ Punk_Api_Project/src/App.tsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.scss";
+import beers from "./assets/beers";
+import "./assets/types";
 
 function App() {
-  const [count, setCount] = useState(0)
+  console.log(beers);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="appContainer">
+      <div className="navMenu">
+        <div className="seachListing">
+          <p>Search by Name</p>
+          <input type="text"></input>
+        </div>
+        <div className="seachListing">
+          <input type="checkbox"></input>
+          <p>Search items go here</p>
+        </div>
+        <div className="searchListing">
+          <input type="checkbox"></input>
+          <p>Acidity</p>
+        </div>
+        <div className="searchListing">
+          <input type="checkbox"></input>
+          <p>% percentage</p>
+        </div>
+        <div className="searchListing">
+          <input type="checkbox"></input>
+          <p>Range</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="listContainer">
+        {beers.map((beer) => (
+          <div className="beerContainer">
+            <p>{beer.name}</p>
+            <img src={beer.image_url} />
+            <p>description</p>
+            <p> --------- </p>
+            <p>{beer.description}</p>
+            <p>pairing</p>
+            <p>---------</p>
+            <p>{beer.food_pairing}</p>
+          </div>
+        ))}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
